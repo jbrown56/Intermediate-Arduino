f008a3abecf005e91029a577b0ceb3dbc03488a3
Justine H Brown <jbrown56@ccs.local>
2019-10-24 12:11:44 -0400

    updating readme and cleaning up old deleted / moved files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-#intermediate arduino, My Intermediate Arduino Assignments hello world button: expansion of lcd backpack using a button LcD Backpack: uses backpack to do commands from hellow world led blink revisited: combines lcd screen, analogwrite, and led to make led fade libraries: contains liquid crystal photo interruptor: uses the photointerrupter to control when led turns on and off pot 3.0: uses potnentiometer with lcd screen and led 
+#intermediate arduino,
+ My Intermediate Arduino Assignments 
+
+hello world button: expansion of lcd backpack using a button LcD Backpack: uses backpack to do
+ commands from hellow world led blink revisited: combines lcd screen, analogwrite, and 
+led to make led fade libraries: contains liquid crystal photo interruptor: uses the 
+photointerrupter to control when led turns on and off pot 3.0: uses potnentiometer with 
+lcd screen and led 
 
--- pot_3.0/pot 3.0.ino
+++ pot_3.0/pot 3.0.ino
@@ -1,39 +1,28 @@
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-int potpin = 2;
-int ledpin = 13;
+#include <LiquidCrystal_I2C.h> 
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display.  
+// If 0x27 doesn't work, try 0x3F.int potpin = 2;
+int ledpin = 11;
 int potvalue;
-LiquidCrystal_I2C lcd(0x3F,16,2);
+int potpin = 2;
+int ledvalue;
 
 void setup(){
 	lcd.begin (16,2); // for 16 x 2 LCD module
 lcd.init();
 lcd.backlight();
 lcd.setBacklight(HIGH);
-lcd.setCursor(0,0);
-lcd.print("brightness:");
-	lcd.println(potvalue);
 Serial.begin(9600);
 }
 
 void loop()
 {
-	lcd.setCursor(0,1);
-	potvalue = analogRead(potpin);
+lcd.setCursor(0,0);
+lcd.print("brightness:");
+lcd.setCursor(0,1);
+lcd.println(ledvalue);
+potvalue = analogRead(potpin);
 Serial.println(potvalue);
-	//if (potvalue < 5 )
-if (potvalue < 100 and potvalue != 0 )
-{
-	analogWrite(ledpin, 50);
-}
-
-if (potvalue > 99 and potvalue < 150){
-    analogWrite(ledpin, 150);
-}
-if(potvalue >149){
-    analogWrite(ledpin, 255);
-}
-if(potvalue = 0){
-    analogWrite(ledpin, 0);
-}
+ledvalue = potvalue/4;
+analogWrite(ledpin, ledvalue);
 }
