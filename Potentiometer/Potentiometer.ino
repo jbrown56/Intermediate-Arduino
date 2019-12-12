f5e6d994f64e4bfbb4a008c6baf5166bf64c25a0
jbrown56 <55801495+jbrown56@users.noreply.github.com>
2019-12-12 12:16:30 -0500

    commented code and added headings

--- Potentiometer/Potentiometer.ino
+++ Potentiometer/Potentiometer.ino
@@ -1,3 +1,7 @@
+//Potentiometer
+//Justine Brown
+//lCD screen reads numerical value representing the brightness of an LED which is controlled by a potentiometer
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 int potpin = 2;
@@ -7,11 +11,11 @@ LiquidCrystal_I2C lcd(0x3F,16,2);
 
 void setup(){
 	lcd.begin (16,2); // for 16 x 2 LCD module
-lcd.init();
+lcd.init();//lcd screen set up
 lcd.backlight();
 lcd.setBacklight(HIGH);
 lcd.setCursor(0,0);
-lcd.print("brightness:");
+lcd.print("brightness:");//lcd print brightness and then the value of the potentiometer
 	lcd.println(potvalue);
 Serial.begin(9600);
 }
@@ -19,18 +23,18 @@ Serial.begin(9600);
 void loop()
 {
 	lcd.setCursor(0,1);
-	potvalue = analogRead(potpin);
+	potvalue = analogRead(potpin);//analog read for value of potentiometer
 Serial.println(potvalue);
 	//if (potvalue < 5 )
-if (potvalue < 100 and potvalue != 0 )
+if (potvalue < 100 and potvalue != 0 )//if the pot value is less than 100 the led will be dul
 {
 	analogWrite(ledpin, 50);
 }
 
-if (potvalue > 99 and potvalue < 150){
+if (potvalue > 99 and potvalue < 150){//if the pot value is in the middle the led will light up a little more
     analogWrite(ledpin, 150);
 }
-if(potvalue >149){
+if(potvalue >149){//if the pot value is high the led will be high
     analogWrite(ledpin, 255);
 }
 if(potvalue = 0){
