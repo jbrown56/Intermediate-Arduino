7040c1e17b57a33655da925720b9f5d2dbc966f5
Justine H Brown <jbrown56@ccs.local>
2020-01-08 12:53:43 -0500

    changed fritzing files

--- Potentiometer/Potentiometer.ino
+++ Potentiometer/Potentiometer.ino
@@ -7,11 +7,11 @@ LiquidCrystal_I2C lcd(0x3F,16,2);
 
 void setup(){
 	lcd.begin (16,2); // for 16 x 2 LCD module
-lcd.init();
+lcd.init();//set up for Lcd
 lcd.backlight();
 lcd.setBacklight(HIGH);
 lcd.setCursor(0,0);
-lcd.print("brightness:");
+lcd.print("brightness:");//lcd is set up to say brightness
 	lcd.println(potvalue);
 Serial.begin(9600);
 }
@@ -19,21 +19,21 @@ Serial.begin(9600);
 void loop()
 {
 	lcd.setCursor(0,1);
-	potvalue = analogRead(potpin);
+	potvalue = analogRead(potpin);//reading the potentiometer pin
 Serial.println(potvalue);
 	//if (potvalue < 5 )
-if (potvalue < 100 and potvalue != 0 )
+if (potvalue < 100 and potvalue != 0 )//led is on low brightness when potentiometer is below 100
 {
 	analogWrite(ledpin, 50);
 }
 
-if (potvalue > 99 and potvalue < 150){
+if (potvalue > 99 and potvalue < 150){//led's brightnes increases a little
     analogWrite(ledpin, 150);
 }
-if(potvalue >149){
+if(potvalue >149){//led is full brightness when potentiometer has high value
     analogWrite(ledpin, 255);
 }
-if(potvalue = 0){
+if(potvalue = 0){//led is off
     analogWrite(ledpin, 0);
 }
 }
